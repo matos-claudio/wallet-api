a254b6e53be02a2ac86409cc4ce4469bd091610e
Claudio Matos <cloud.devmob@gmail.com>
2021-09-13 23:22:36 -0300

    Criando User Entity

---
+++ src/test/java/com/wallet/repository/UserRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.wallet.repository;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.wallet.entity.User;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserRepositoryTest {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Test
+	public void testSave() {
+		User user = new User();
+		user.setName("Teste");
+		user.setPassword("123456");
+		user.setEmail("teste@teste.com");
+		User response = userRepository.save(user);
+		
+		assertNotNull(response);
+	}
+}
