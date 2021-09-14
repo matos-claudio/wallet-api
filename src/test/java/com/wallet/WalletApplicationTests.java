1dd71b51b685c63c3819b6d9b38209963a5c220a
Claudio Matos <cloud.devmob@gmail.com>
2021-09-14 10:39:23 -0300

    Finalizado repositorio do usuario

--- src/test/java/com/wallet/WalletApplicationTests.java
+++ src/test/java/com/wallet/WalletApplicationTests.java
@@ -8,8 +8,8 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class WalletApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
--- src/test/java/com/wallet/repository/UserRepositoryTest.java
+++ src/test/java/com/wallet/repository/UserRepositoryTest.java
@@ -1,7 +1,13 @@
 package com.wallet.repository;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Optional;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +20,24 @@ import com.wallet.entity.User;
 @SpringBootTest
 public class UserRepositoryTest {
 	
+	private static final String EMAIL = "set@gmail.com";
+	
 	@Autowired
 	UserRepository userRepository;
+	
+	@Before
+	public void setUp() {
+		User user = new User();
+		user.setName("SetupUser");
+		user.setPassword("1234");
+		user.setEmail(EMAIL);
+		userRepository.save(user);
+	}
+	
+	@After
+	public void tearDown() {
+		userRepository.deleteAll();
+	}
 	
 	@Test
 	public void testSave() {
@@ -26,5 +48,11 @@ public class UserRepositoryTest {
 		User response = userRepository.save(user);
 		
 		assertNotNull(response);
+	}
+	
+	public void testFindByEmail() {
+		Optional<User> response = userRepository.findByEmailEquals(EMAIL);
+		assertTrue(response.isPresent());
+		assertEquals(response.get().getEmail(), EMAIL);
 	}
 }
